e49267e43cd26b525f2c67609a0eeb76204cdbaa
flavio mattos <frmmf@academy.cin.ufpe.br>
2019-03-24 15:39:23 -0300

    restart screen

--- wwdc19.playground/Contents.swift
+++ wwdc19.playground/Contents.swift
@@ -3,7 +3,7 @@ import PlaygroundSupport
 import SpriteKit
 
 // Load the SKScene from 'GameScene.sks'
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 768, height: 1024))
+let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 384, height: 512))
 if let scene = GameScene(fileNamed: "GameScene") {
     //Set; the; scale mode to scale to fit the window
     scene.scaleMode = .aspectFit
--- wwdc19.playground/Sources/GameScene.swift
+++ wwdc19.playground/Sources/GameScene.swift
@@ -12,6 +12,10 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
     var player = SKSpriteNode()
     var leftWall = SKSpriteNode()
     var rightWall = SKSpriteNode()
+    var youLose = SKSpriteNode()
+    var youWin = SKSpriteNode()
+    var allBlackBg = SKSpriteNode()
+    var retry = SKSpriteNode()
     var counter:Int = 0
     var index = 0
     var playerLife = 5
@@ -45,7 +49,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
             return true
         }
         return false
-        }
+    }
     
     //Number counter to start
     func restartTimer() {
@@ -54,7 +58,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
             self.numbers?.texture = SKTexture(imageNamed: "num\(self.number).png")
             self.number -= 1
             if(self.number == 0){
-               self.numbers!.run(SKAction.fadeOut(withDuration: 0.5))
+                self.numbers!.run(SKAction.fadeOut(withDuration: 0.5))
             }else{
                 self.restartTimer()
             }
@@ -64,7 +68,12 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override public func sceneDidLoad() {
+        //finish screen
+        self.youLose = childNode(withName: "youLose") as! SKSpriteNode
+        self.retry = childNode(withName: "retry") as! SKSpriteNode
+        self.allBlackBg = childNode(withName: "allBlackBg") as! SKSpriteNode
         
+        //numbers caller
         self.numbers = self.childNode(withName: "numbers") as? SKSpriteNode
         restartTimer()
         
@@ -75,7 +84,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
         //nodes physicsBody
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         ball.physicsBody?.categoryBitMask = contactMaskType.ball
-
+        
         ball.physicsBody?.collisionBitMask = contactMaskType.player | contactMaskType.enemy
         ball.physicsBody?.contactTestBitMask = contactMaskType.player | contactMaskType.enemy
         
@@ -86,8 +95,8 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
         player = self.childNode(withName: "player") as! SKSpriteNode
         player.physicsBody?.categoryBitMask = contactMaskType.player
         player.physicsBody?.collisionBitMask = contactMaskType.ball
-
-       
+        
+        
         leftWall = self.childNode(withName: "leftWall") as! SKSpriteNode
         
         
@@ -100,10 +109,10 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
             //ball direction
             let startBall = Int.random(in: 0...1)
             if startBall == 0 {
-            self.ball.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: -30, dy: -30))
             } else {
-            self.ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
-               
+                self.ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
+                
                 //start background music
                 do{
                     self.soundBg = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "bgmStage", ofType: "wav")!))
@@ -115,15 +124,15 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
             }
         }
     }
- 
+    
     //changing player color
     @objc func playerColorChange(){
-    
+        
         padColorIndex = padColorIndex + 1
         if padColorIndex >= 4 {
             padColorIndex = 0
         }
-            player.texture = padcolor[padColorIndex]
+        player.texture = padcolor[padColorIndex]
     }
     override public func didMove(to view: SKView) {
     }
@@ -166,7 +175,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
             scene!.addChild(shockwave)
             
             shockwave.run(shockWaveAction)
-           
+            
             counter += 1
             self.hitcounter()
             
@@ -180,20 +189,24 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     public override  func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.playertouch = true
-//        for touch in touches{
-//
-//            //let location = touch.location(in: self)
-////            player.run(SKAction.moveTo(x: location.x, duration: 0.2))
-//        }
+        for t in touches{
+            let node = self.nodes(at: t.location(in: self))
+            if node.contains(self.retry){
+                let reveal = SKTransition.crossFade(withDuration: 1.5)
+                if let gameScene = GameScene(fileNamed: "GameScene"){
+                    gameScene.scaleMode = .aspectFit
+                    self.view?.presentScene(gameScene, transition:reveal)
+                }
+            }
+        }
     }
-    
+    // here are set the touch and drag functions
     public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("chamou moved")
         for touch in touches{
             self.playertouch = true
             let location = touch.location(in: self)
             player.run(SKAction.moveTo(x: location.x, duration: 0.2))
-          
+            
             //range limit of player movement
             let xrange = SKRange(lowerLimit: -((scene?.size.width)!/2) + 150, upperLimit: ((scene?.size.width)!/2 - 150))
             let yrange = SKRange(lowerLimit: -320, upperLimit: -320)
@@ -212,7 +225,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         //Enemy movement
-        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 0.5))
+        enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1))
         
         //ball move corrector
         if ball.position.y > 0 && abs(ball.physicsBody!.velocity.dy) <= 80.0 {
@@ -228,10 +241,10 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
             ball.physicsBody?.applyForce(CGVector(dx: 80, dy: 0))
         }
         
-        //winner detector
+        //here dectect if the ball passes through the player's pad new functions will be called
         if ball.position.y <= player.position.y - 50  {
-           
-            //player life is done
+            
+            //enemy life are reduced
             self.childNode(withName: "PlayerLife\(playerLife)")?.run(SKAction.fadeOut(withDuration: 0.5))
             playerLife -= 1
             
@@ -241,35 +254,44 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
                 self.ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
             }
             if playerLife < 1{
+                physicsWorld.speed = 0
+                self.youLose.texture = SKTexture(imageNamed: "youlose")
+                self.allBlackBg.run(SKAction.fadeIn(withDuration: 0.5)) {
+                    self.youLose.run(SKAction.fadeIn(withDuration: 0.5), completion: {
+                        self.retry.run(SKAction.fadeIn(withDuration: 0.5))
+                    })
+                }
             }
         }
+        //here dectect if the ball passes through the enemy's pad new functions will be called
         else if ball.position.y >= enemy.position.y + 50 {
             
-            //enemy life is done
+            //enemy life are reduced
             self.childNode(withName: "EnemyLife\(enemyLife)")?.run(SKAction.fadeOut(withDuration: 0.5))
             enemyLife -= 1
-           
+            
             ball.position = CGPoint(x: 0, y: 0)
             ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
             DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 self.ball.physicsBody?.applyImpulse(CGVector(dx: 30, dy: 30))
             }
+            if enemyLife < 1{
+                
+                physicsWorld.speed = 0
+                self.youLose.texture = SKTexture(imageNamed: "winner")
+                self.allBlackBg.run(SKAction.fadeIn(withDuration: 0.5)) {
+                    self.youLose.run(SKAction.fadeIn(withDuration: 0.5), completion: {
+                        self.retry.run(SKAction.fadeIn(withDuration: 0.5))
+                    })
+                }
+            }
         }
     }
     
-    //ball image changer
-    /// <#Description#>
+    //here the counter variable change modify the ball color
     func hitcounter() {
         
-        if counter == 0 || counter == 1 {
-            ball.texture = ballcolor[3]
-            index = 3
-        }
-        if counter == 2 {
-            ball.texture = ballcolor[3]
-            index = 3
-        }
-        if counter == 3 {
+        if counter >= 0 && counter <= 3 {
             ball.texture = ballcolor[3]
             index = 3
         }
@@ -280,6 +302,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate {
             leftWall.physicsBody?.restitution = 1.1
             rightWall.physicsBody?.restitution = 1.1
         }
+        //enemy pad receive the color texture
         enemy.texture = padcolor[index]
     }
 }
